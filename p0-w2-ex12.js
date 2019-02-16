9f5f8c415e58b022391f4db216dbeb58a22083cf
Faqih Arfiyanto <faqiharfiyanto@gmail.com>
2019-02-16 13:42:11 +0700

    edit coding

--- p0-w2-ex12.js
+++ p0-w2-ex12.js
@@ -1,17 +1,20 @@
 function konversiMenit(menit) {
-    var tampung = ''
-    if( menit < 60 ) {
-        tampung += '0:' + ('0' + menit).slice(-2)
-        return tampung
-    }
-    else if( menit < 120) {
-        tampung += '1:' + ('0' + (menit - 60)).slice(-2)
-        return tampung
-    }
-    else {
-        tampung += '2:' + ('0' + (menit - 120)).slice(-2)
-        return tampung
-    }
+    /*
+    menit = 63
+    menit dibagi 60= 1 lalu hasilnya dibuletin ke bawah INI JAMNYA
+    menit modulo 60 = 3 INI MENITNYA
+    jamnya diubah ke string terus tambah :
+    menitnya diubah kestring trus tambah nol di depannya pake slice -2 biar ga numpung jadi 3 angka
+    */
+   var jamAngka = Math.floor(menit / 60)
+   var menitAngka = menit % 60
+   var jamString = jamAngka.toString() + ':'
+   var menitString = ('0' + menitAngka).slice(-2)
+   var result = jamString + menitString
+
+   return result
+
+
 }
   
   // TEST CASES
@@ -20,3 +23,4 @@ function konversiMenit(menit) {
   console.log(konversiMenit(53)); // 0:53
   console.log(konversiMenit(88)); // 1:28
   console.log(konversiMenit(120)); // 2:00
+  console.log(konversiMenit(1000))
