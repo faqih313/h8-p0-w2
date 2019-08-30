5abd27c5c4a8ebfba6356daeecd00909e0d9a3ad
Faqih Arfiyanto <faqiharfiyanto@gmail.com>
2019-08-30 10:17:25 +0700

    modifiying exercise 5

--- p0-w2-ex5.js
+++ p0-w2-ex5.js
@@ -1,26 +1,29 @@
-var word = 'JavaScript';
-var second = 'is';
-var third = 'awesome';
-var fourth = 'and';
-var fifth = 'I';
-var sixth = 'love';
-var seventh = 'it!';
+//NOMOR 1
+var word = 'JavaScript'
+var second = 'is'
+var third = 'awesome'
+var fourth = 'and'
+var fifth = 'I'
+var sixth = 'love'
+var seventh = 'it!'
 
-console.log(word + ' ' + second + ' ' + third + ' ' + fourth + ' ' + fifth + ' ' + sixth + ' ' + seventh)
+console.log(word+' '+second+' '+third+' '+fourth+' '+fifth+' '+sixth+' '+seventh)
 
-var word2 = 'wow JavaScript is so cool';
-var exampleFirstWord2 = word2[0] + word2[1] + word2[2];
-var secondWord2 = word2[4] + word2[5] + word2[6] + word2[7] + word2[8] + word2[9] + word2[10] +word2[11] + word2[12] + word2[13]
-var thirdWord2 = word2[15] + word2[16] 
-var fourthWord2 = word2[18] + word2[19]
-var fifthWord2 = word2[21] + word2[22] + word2[23] + word2[24]
+//NOMOR 2
+var word2 = 'wow JavaScript is so cool'
+var exampleFirstWord2 = word2[0]+word2[1]+word2[2]
+var secondWord2 = word2[4]+word2[5]+word2[6]+word2[7]+word2[8]+word2[9]+word2[10]+word2[11]+word2[12]+word2[13]
+var thirdWord2 = word2[15]+word2[16]
+var fourthWord2 = word2[18]+word2[19]
+var fifthWord2 = word2[21]+word2[22]+word2[23]+word2[24]
 
-console.log('First Word: ' + exampleFirstWord2);
-console.log('Second Word: ' + secondWord2);
-console.log('Third Word: ' + thirdWord2);
-console.log('Fourth Word: ' + fourthWord2);
-console.log('Fifth Word: ' + fifthWord2);
+console.log('First Word: '+exampleFirstWord2)
+console.log('Second Word: '+secondWord2)
+console.log('Third Word: '+thirdWord2)
+console.log('Fourth Word: '+fourthWord2)
+console.log('Fifth Word: '+fifthWord2)
 
+//NOMOR 3
 var word3 = 'wow JavaScript is so cool';
 var exampleFirstWord3 = word3.substring(0,3);
 var secondWord3 = word3.substring(4,14)
@@ -28,12 +31,13 @@ var thirdWord3 = word3.substring(15,17)
 var fourthWord3 = word3.substring(18,20)
 var fifthWord3 = word3.substring(21)
 
-console.log('First Word: ' + exampleFirstWord3);
-console.log('Second Word: ' + secondWord3);
-console.log('Third Word: ' + thirdWord3);
-console.log('Fourth Word: ' + fourthWord3);
-console.log('Fifth Word: ' + fifthWord3);
+console.log('First Word: '+exampleFirstWord3);
+console.log('Second Word: '+secondWord3);
+console.log('Third Word: '+thirdWord3);
+console.log('Fourth Word: '+fourthWord3);
+console.log('Fifth Word: '+fifthWord3);
 
+//NOMOR 4
 var word4 = 'wow JavaScript is so cool';
 var exampleFirstWord4 = word4.substring(0,3);
 var secondWord4 = word4.substring(4,14)
@@ -47,8 +51,8 @@ var thirdWord4Length = thirdWord4.length;
 var fourthWord4Length = fourthWord4.length;
 var fifthWord4Length = fifthWord4.length;
 
-console.log('First Word: ' + exampleFirstWord4 + ', with length: ' + firstWordLength);
-console.log('Second Word: ' + secondWord4 + ', with length: ' + secondWord4Length);
-console.log('Third Word: ' + thirdWord4 + ', with length: ' + thirdWord4Length);
-console.log('Fourth Word: ' + fourthWord4 + ', with length: ' + fourthWord4Length);
-console.log('Fifth Word: ' + fifthWord4 + ', with length: ' + fifthWord4Length);
+console.log('First Word: '+exampleFirstWord4+', with length: '+firstWordLength);
+console.log('Second Word: '+secondWord4+', with length: '+secondWord4Length);
+console.log('Third Word: '+thirdWord4+', with length: '+thirdWord4Length);
+console.log('Fourth Word: '+fourthWord4+', with length: '+fourthWord4Length);
+console.log('Fifth Word: '+fifthWord4+', with length: '+fifthWord4Length);
