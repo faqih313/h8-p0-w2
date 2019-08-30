5e8adbbe4951908b334230e6f412c0d53d84f782
Faqih Arfiyanto <faqiharfiyanto@gmail.com>
2019-08-30 16:56:15 +0700

    modifiying exercise 7

--- p0-w2-ex7.js
+++ p0-w2-ex7.js
@@ -1,24 +1,25 @@
 //NOMOR 1
 var rows1 = 5
-for(var i = 1; i <= rows1; i++) {
+for(var i=0; i<rows1; i++) {
     console.log('*')
 }
 
 //NOMOR 2
 var rows2 = 5
 
-for(var i = 1; i <= rows2; i++) {
-    result = ''
-    for(var j = 1; j<=rows2 ; j++) {
-        result += '*'   
-    } console.log(result)
+for(var i=0; i<rows2; i++) {
+    var result = ''
+    for(var j=0; j<rows2 ; j++) {
+        result += '*'
+    }
+    console.log(result)
 }
 
 //NOMOR 3
 var rows3 = 5
 var result = ''
 
-for( var i = 1; i <= rows3; i++ ) {
+for( var i=0; i<rows3; i++ ) {
     result += '*'
     console.log(result)
 }
