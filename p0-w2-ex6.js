9e8ad6d86af377f777c34d0ceb14aaad122a2f2c
Faqih Arfiyanto <faqiharfiyanto@gmail.com>
2019-08-30 16:09:16 +0700

    modifiying exercise 6

--- p0-w2-ex6.js
+++ p0-w2-ex6.js
@@ -1,60 +1,63 @@
 //NOMOR 1
-var i = 2
-console.log('LOOPING PERTAMA')
-while(i <= 20) {
-    console.log(i + ' - I love coding')
-    i += 2
-}
+var angka = 2
 
-var i2 = 20
-console.log('LOOPING KEDUA')
-while(i2 >= 2) {
-    console.log(i2 + ' - I will become fullstack developer')
-    i2 -= 2
+console.log('LOOPING PERTAMA NOMOR 1')
+while (angka<19) {
+    console.log(String(angka),'- I love coding')
+    angka+=2
+}
+console.log(String(angka),'- I love coding')
+
+console.log('LOOPING KEDUA NOMOR 2')
+while (angka>1) {
+    console.log(String(angka),'- I will become fullstack developer')
+    angka-=2
 }
 
 //NOMOR 2
 console.log('LOOPING PERTAMA NOMOR 2')
-for(var i = 1; i <= 20; i++) {
-    console.log(i + ' - I love coding')
+for(var angka=1; angka<21; angka++) {
+    console.log(String(angka),'- I love coding')
 }
 
 console.log('LOOPING KEDUA NOMOR 2')
-for(var i = 20; i >=1; i--) {
-     console.log(i + ' - I will become fullstack developer')
+for(angka--; angka>0; angka--) {
+     console.log(String(angka),'- I will become fullstack developer')
 }
 
 //NOMOR 3
 console.log('CASE PERTAMA NOMOR 3')
-for (var i = 1; i <= 100; i++) {
-    if (i % 2 === 0) {
-      console.log('"GENAP"')
+for (var i=1; i<101; i++) {
+    if (i%2===0) {
+      console.log('GENAP')
     } else {
-      console.log('"GANJIL"')
+      console.log('GANJIL')
     }
   }
 
 console.log('CASE KEDUA NOMOR 3')
-for (var i = 1; i <= 100; i += 2) {
-  if (i % 3 === 0) {
-    console.log('"3 KELIPATAN 3"')
+for (var i=1; i<101; i+=2) {
+  if (i%3 === 0) {
+    console.log(String(i),'KELIPATAN 3')
   } else {
-    console.log('""')
+    console.log('')
   }
 }
 
-for (var i = 1; i <= 100; i += 5) {
-  if (i % 6 === 0) {
-    console.log('"6 KELIPATAN 6"')
+console.log('CASE KETIGA NOMOR 3')
+for (var i=1; i<101; i+=5) {
+  if (i%6 === 0) {
+    console.log(String(i),'KELIPATAN 6')
   } else {
-    console.log('""')
+    console.log('')
   }
 }
 
-  for (var i = 1; i <= 100; i += 9) {
-    if (i % 10 === 0) {
-      console.log('"10 KELIPATAN 10"')
-    } else {
-      console.log('""')
-    }
+console.log('CASE KEEMPAT NOMOR 3')
+for (var i=1; i<101; i+=9) {
+  if (i%10 === 0) {
+    console.log(String(i),'KELIPATAN 10')
+  } else {
+    console.log('')
   }
+}
